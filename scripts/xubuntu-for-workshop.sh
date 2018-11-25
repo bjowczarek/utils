454f56fe9e0c9bcf7982b9ad96c1b6b41eeed1dc
Bartosz Owczarek <bowczarek@Bartoszs-MacBook-Pro.local>
2018-11-25 22:32:30 +0100

    changes

--- scripts/xubuntu-for-workshop.sh
+++ scripts/xubuntu-for-workshop.sh
@@ -2,6 +2,5 @@ apt-get install virtualbox-guest-x11
 apt-get purge thunderbird*
 apt-get purge libreoffice
 apt-get install git-core
-add-apt-repository ppa:linuxuprising/java
 apt-get update
-apt-get install oracle-java11-installer
+apt-get install openjdk-11-jdk
